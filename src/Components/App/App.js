8e14317594d6cfc640c76f539386cf0eedfcaa62
mdflynn <mdflynn34@outlook.com>
2021-01-02 19:09:52 -0700

    Modularize Components

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import space from '../../assets/space.jpg';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Personal Site</h1>
+    </div>
+  );
+}
+
+export default App;
+
