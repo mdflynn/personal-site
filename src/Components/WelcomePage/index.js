02c63d244b3ce2bc4348a50f91f794f0295fa207
mdflynn <mdflynn34@outlook.com>
2021-01-25 16:20:36 -0700

    General structure styling

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./WelcomePage.scss";
+import mike from "../../assets/headshot_nobackground.png";
 
 const WelcomePage = () => {
-    return (
-        <main>
-            Welcome Page
-        </main>
-    )
-}
+  return (
+    <main className="welcome-main">
+      <section className="left-main">
+        <p className="name">
+          Mike
+          <br />
+          Flynn
+        </p>
+        <img className="headshot" src={mike} />
+      </section>
+      <section className="right-main"></section>
+    </main>
+  );
+};
 
 export default WelcomePage;
