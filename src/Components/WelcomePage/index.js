8c15f60ddaf7ef180d4c423e9431ba64b3a321d7
mdflynn <mdflynn34@outlook.com>
2021-01-25 18:40:13 -0700

    add entrance handling function

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -4,32 +4,49 @@ import mike from "../../assets/headshot_nobackground.png";
 
 const WelcomePage = () => {
   const [load, setLoad] = useState(false);
+  const [newClass, setNewClass] = useState("entrance");
 
   const displayMain = () => {
     return (
       <section className="left-main">
-        <p className="name">
-          Mike
-          <br />
-          Flynn
-        </p>
-        <img className="headshot" src={mike} alt="Mike Headshot"/>
+        <div className="img-div">
+          <p className="name">
+            Mike
+            <br />
+            Flynn
+          </p>
+          <img className="headshot" src={mike} alt="Mike Headshot" />
+        </div>
       </section>
     );
   };
 
-  const displayEntrance = () => {
-      return (
-          <section className="left-main">
-              <p className='entrance' onAnimationEnd={() => setLoad(true)}>Hi, I'm Mike</p>
-          </section>
-      )
+  const handleEntranceAnimation = () => {
+    setNewClass("exit");
+    setInterval(() => {
+      setLoad(true);
+    }, 2000);
   }
+
+  const displayEntrance = () => {
+    return (
+      <section className="left-main-entrance">
+        <p
+          className={newClass}
+          onAnimationEnd={() => {
+            handleEntranceAnimation()
+          }}
+        >
+          Hi, I'm Mike
+        </p>
+      </section>
+    );
+  };
 
   return (
     <main className="welcome-main">
-     {!load && displayEntrance()}
-     {load && displayMain()}
+      {!load && displayEntrance()}
+      {load && displayMain()}
       <section className="right-main"></section>
     </main>
   );
