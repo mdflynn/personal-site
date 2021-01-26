39303228cf797a669751e625d7142303e3eb3d12
mdflynn <mdflynn34@outlook.com>
2021-01-26 13:48:53 -0700

    Add boilerplate returns to new comps

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,6 +1,10 @@
 import './App.scss';
 import WelcomePage from '../WelcomePage';
 import Nav from '../Nav';
+import About from '../About';
+import Portfolio from '../Portfolio';
+import Interests from '../Interests';
+import Contact from '../Contact';
 import { Route, Switch} from 'react-router-dom';
 
 function App() {
@@ -9,6 +13,10 @@ function App() {
       <Nav />
       <Switch>
         <Route exact path='/' component={WelcomePage} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/interests' component={Interests} />
+        <Route exact path='/contact' component={Contact} />
       </Switch>
     </>
   );
