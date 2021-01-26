44b8072cc4d79d183893c5898133793b68236b08
Mike Flynn <64620860+mdflynn@users.noreply.github.com>
2021-01-26 12:55:56 -0700

    Feature/welcome style (#5)
    * add entrance handling function
    
    * Add animations to entrance
    
    * Add additional variable
    
    * Add contact styling and side info
    
    * Add additional font
    
    * update variables
    
    * Add contact display
    
    * formatting

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -4,33 +4,70 @@ import mike from "../../assets/headshot_nobackground.png";
 
 const WelcomePage = () => {
   const [load, setLoad] = useState(false);
+  const [newClass, setNewClass] = useState("entrance");
 
   const displayMain = () => {
     return (
       <section className="left-main">
-        <p className="name">
-          Mike
-          <br />
-          Flynn
-        </p>
-        <img className="headshot" src={mike} alt="Mike Headshot"/>
+        <div className="img-div">
+          <p className="name">
+            Mike
+            <br />
+            Flynn
+          </p>
+          <img className="headshot" src={mike} alt="Mike Headshot" />
+        </div>
       </section>
     );
   };
 
+  const handleEntranceAnimation = () => {
+    setNewClass("exit");
+    setInterval(() => {
+      setLoad(true);
+    }, 2000);
+  };
+
   const displayEntrance = () => {
-      return (
-          <section className="left-main">
-              <p className='entrance' onAnimationEnd={() => setLoad(true)}>Hi, I'm Mike</p>
-          </section>
-      )
-  }
+    return (
+      <section className="left-main-entrance">
+        <p
+          className={newClass}
+          onAnimationEnd={() => {
+            handleEntranceAnimation();
+          }}
+        >
+          Hi, I'm Mike
+        </p>
+      </section>
+    );
+  };
+
+  const displaySide = () => {
+    return (
+      <>
+        <section className="right-main">
+          <p className="title">Software Engineer</p>
+          <p className="location">Basend in Denver,</p>
+          <p className="intro">I'm a developer and fitness enthusiast.</p>
+          <p className="welcome-contact">
+            P: (970) 456-6128
+            <br />
+            E:{" "}
+            <a className="email-link" href="mailto:mdflynn34@outlook.com">
+              mdflynn34@outlook.com
+            </a>
+          </p>
+        </section>
+      </>
+    );
+  };
 
   return (
     <main className="welcome-main">
-     {!load && displayEntrance()}
-     {load && displayMain()}
-      <section className="right-main"></section>
+      {!load && displayEntrance()}
+      {load && displayMain()}
+      {load && displaySide()}
     </main>
   );
 };
