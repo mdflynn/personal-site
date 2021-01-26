6887c89ddd955c8b5e4b672c5b044354f2e20cc4
mdflynn <mdflynn34@outlook.com>
2021-01-26 15:46:28 -0700

    Add Nav unit tests

---
+++ src/Components/Nav/Nav.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Nav from "./index";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Nav", () => {
+  it("should render the Nav", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    const emblem = screen.getByText("MF");
+    expect(emblem).toBeInTheDocument();
+  });
+  it("should render nav links", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    const home = screen.getByText("Home");
+    const about = screen.getByText("About");
+    const portfolio = screen.getByText("Portfolio");
+    const interests = screen.getByText("Interests");
+    const contact = screen.getByText("Contact");
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(portfolio).toBeInTheDocument();
+    expect(interests).toBeInTheDocument();
+    expect(contact).toBeInTheDocument();
+  });
+});
