a731c05d346419f28b6156deea454f3121c6f72a
mdflynn <mdflynn34@outlook.com>
2021-01-27 16:54:39 -0700

    Add outline of content

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -5,3 +5,37 @@ const About = () => {
 };
 
 export default About;
+//talk about career and education up to this point
+//break down resume and decisions
+
+//Wells Fargo
+//After I graduated UNC, I spent a few months selling Hyundais before I found my
+//first career oriented job. I started off at Wells Fargo Financial. I spent a year commuting back and forth from 
+//Fort Collins, CO to Cheyenne, WY. During this role, I focused on generating
+//appointments and leads in order to offer a customer a wide range of credit offerings. I received a 
+//District Employee of the Month award for the success I was having in the role. I transfered over to the banking
+//side eventually, working my way up from a teller to a personal banker. I consistently met and surpassed 
+//rigorous sales goals while ensuring customer's were set up in the best available banking situation.
+
+//Prague
+//After I spent roughly three years working for Wells Fargo at varying locations, I decided that I wasn't happy
+//where I was and how my career was shaping up. I didn't feel strongly about my job and that it would
+//bring my the type of challenges I was looking for. I ended up quitting, casing out my 401(k) plan and moving 
+//Prague, Czech Republic to get certified in Teaching English as a Foregin Language (TEFL). Due to Eyjafjallajökull 
+//in Iceland erupting right when we were supposed to fly to Europe, this trip only lasted four months. I was able
+//to earn my certification, do a bit of teaching, and explore central and eastern Europe. It was a life-changing
+//experience
+
+//Steamboat -> MBA
+//After I came back from Europe, I spent some time worker as a teller and snowboarding as much as possible living in 
+//Steamboat Springs, CO. I needed to kill some time before I went to my MBA program in the following summer. It was 
+//a season with heavy snow and great runs. It was an amazing experience, both on the mountain and in the community. 
+//During that time, I was able to campaign and win the title of Steamboat's Most Eligible Bachelor; my crowning 
+//achievement! I then spent the next two years living in Denver and attending the University of Colorado at Denver. 
+//I was to graduate with my MBA that focuses on International Business. 
+
+//Intrado
+//After my MBA, I worked at Intrado for three years. A company that's in the public safety and telecommunication industry.
+//I worked my way up to a Senior Implementation Specialist. I handle projects that ranged in size and focused on 
+//implementing and decommissioning cellular site's 911 data and routing. Ensuring that 911 calls were properly routed to
+//the nearest PSAP (Public Safety Answering Point).
