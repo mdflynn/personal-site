5edbe59e2c06adbe4169ac65198fea7b81854952
mdflynn <mdflynn34@outlook.com>
2021-02-01 13:02:04 -0700

    Add react slideshow

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./Portfolio.scss";
+import { Zoom } from "react-slideshow-image";
+
+import denver from "../../assets/denver.jpg";
+import wellsFargo from "../../assets/wellsFargo.jpg";
+import prague from "../../assets/prague.jpg";
 
 const Portfolio = () => {
-  return <h1>Portfolio</h1>;
+  const images = [denver, wellsFargo, prague];
+
+  const zoomInProperties = {
+    indicators: true,
+    scale: 1.4,
+  };
+
+  const generateSlideshow = () => {
+    return images.map((each, index) => (
+      <div key={index} className="slide-div">
+        <img className="slide-img" src={each} alt="Portfolio" />
+      </div>
+    ));
+  };
+
+  return (
+    <div>
+      <Zoom {...zoomInProperties}>{generateSlideshow()}</Zoom>
+    </div>
+  );
 };
 
 export default Portfolio;
