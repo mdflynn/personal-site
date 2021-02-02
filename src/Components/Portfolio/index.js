3ea3281c549476eb28fdeef6b76a4c9316296af2
Mike Flynn <64620860+mdflynn@users.noreply.github.com>
2021-02-01 20:28:28 -0700

    Feature/portfolio design (#17)
    * Update images
    
    * Add react slideshow
    
    * Initial commit
    
    * seperate out function for more control

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Portfolio.scss";
+import { Zoom } from "react-slideshow-image";
+
+import rancid from "../../assets/rancid.png";
+import gamesleuth from "../../assets/gamesleuth.png";
+import prague from "../../assets/prague.jpg";
 
 const Portfolio = () => {
-  return <h1>Portfolio</h1>;
+  const [load, setLoad] = useState(false);
+
+  // const images = [gamesleuth, rancid, prague];
+
+  const zoomInProperties = {
+    indicators: true,
+    scale: 1.4,
+  };
+
+  //animation for portfolio, then setLoad to generate slideshow
+
+  return (
+    <div className="portfolio-container">
+      <Zoom {...zoomInProperties}>
+      <div key={0} className="slide-div">
+        <h1 className="portfolio-title">Game Sleuth</h1>
+        <img className="slide-img" src={gamesleuth} alt="Game Sleuth" />
+      </div>
+      <div key={1} className="slide-div">
+        <h1 className="portfolio-title">Rancid Tomatillos</h1>
+        <img className="slide-img" src={rancid} alt="Rancid Tomatillos" />
+      </div>
+      <div key={2} className="slide-div">
+        <h1 className="portfolio-title">Project 3</h1>
+        <img className="slide-img" src={prague} alt="Portfolio" />
+      </div>
+      </Zoom>
+    </div>
+  );
 };
 
 export default Portfolio;
