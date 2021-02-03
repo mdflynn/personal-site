a6c0b9e9f85c5c6263555058082efd98cb269966
mdflynn <mdflynn34@outlook.com>
2021-02-02 19:56:57 -0700

    Add twitter svg

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -3,7 +3,7 @@ import ContactForm from "../ContactForm";
 import phone from "../../assets/phone.svg";
 import message from "../../assets/message.svg";
 import linkedin from "../../assets/linkedin.svg";
-import github from "../../assets/github.svg";
+import twitter from "../../assets/twitter.svg";
 import "./Contact.scss";
 
 const Contact = () => {
@@ -14,7 +14,7 @@ const Contact = () => {
         <img className="icon" src={phone} alt="phone" />
         <img className="icon" src={message} alt="message" />
         <img className="icon" src={linkedin} alt="linkedin" />
-        <img className="icon" src={github} alt="github" />
+        <img className="icon" src={twitter} alt="twitter" />
       </section>
      <ContactForm />
     </main>
@@ -23,7 +23,6 @@ const Contact = () => {
 
 export default Contact;
 //linkedin
-//github
 //twitter
 //email
 //phone
