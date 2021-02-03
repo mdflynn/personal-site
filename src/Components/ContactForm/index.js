1b62ffb3ef730780b6e6097625df1062e1162660
mdflynn <mdflynn34@outlook.com>
2021-02-02 18:59:43 -0700

    Add form functionality

--- src/Components/ContactForm/index.js
+++ src/Components/ContactForm/index.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactForm.scss";
 
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleNameChange = (event) => {
+    const { value } = event.target;
+    setName(value);
+  };
+
+  const handleEmailChange = (event) => {
+    const { value } = event.target;
+    setEmail(value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    const { value } = event.target;
+    setPhoneNumber(value);
+  };
+
   return (
-    <form>
-      <input className="input" placeholder="Name" type="text" required="" />
-      <input className="input" placeholder="Email" type="email" name="customerEmail" />
+    <form onSubmit={handleSubmit}>
+      <input
+        className="input"
+        placeholder="Name"
+        type="text"
+        name="name"
+        value={name}
+        required=""
+        onChange={handleNameChange}
+      />
+      <input
+        className="input"
+        placeholder="Email"
+        type="email"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
       <input
         className="input"
         placeholder="Phone"
         type="tel"
-        name="customerPhone"
+        name="phoneNumber"
+        value={phoneNumber}
         pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        onChange={handlePhoneNumberChange}
       />
       <button className="contact-btn">Contact</button>
     </form>
