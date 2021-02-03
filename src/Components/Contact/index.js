c6c94fa0680987465474e0ca36ffe792b8611ec2
mdflynn <mdflynn34@outlook.com>
2021-02-02 20:21:23 -0700

    Add more styling

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -8,15 +8,39 @@ import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <main>
+    <main className="contact-main">
       <section className="contact">
         <h1>Contact</h1>
-        <img className="icon" src={phone} alt="phone" />
-        <img className="icon" src={message} alt="message" />
-        <img className="icon" src={linkedin} alt="linkedin" />
-        <img className="icon" src={twitter} alt="twitter" />
+        <div className="contact-div">
+          <img className="icon" src={phone} alt="phone" />
+          <div className="details">
+            <p>Call Me Now</p>
+            <p>970 456 6128</p>
+          </div>
+        </div>
+        <div className="contact-div">
+          <img className="icon" src={message} alt="message" />
+          <div className="details">
+            <p>Send Me An Email</p>
+            <p>mdflynn34@outlook.com</p>
+          </div>
+        </div>
+        <div className="contact-div">
+          <img className="icon" src={linkedin} alt="linkedin" />
+          <div className="details">
+            <p>Reach Out On LinkedIn</p>
+            <p>LinkedIn</p>
+          </div>
+        </div>
+        <div className="contact-div">
+          <img className="icon" src={twitter} alt="twitter" />
+        <div className="details">
+          <p>DM me on Twitter</p>
+          <p>Twitter</p>
+        </div>
+        </div>
       </section>
-     <ContactForm />
+      <ContactForm />
     </main>
   );
 };
