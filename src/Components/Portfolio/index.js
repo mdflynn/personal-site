5629fe8b20194c521ac6d73a7b0f34e7d9ef8a97
mdflynn <mdflynn34@outlook.com>
2021-02-04 20:28:06 -0700

    Add videos

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,34 +1,45 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import "./Portfolio.scss";
 import { Zoom } from "react-slideshow-image";
 
-import rancid from "../../assets/rancid.png";
-import gamesleuth from "../../assets/gamesleuth.png";
+
 import prague from "../../assets/night-prague.jpg";
 
 const Portfolio = () => {
-  const zoomInProperties = {
-    indicators: true,
-    scale: 1.4,
-  };
 
   //animation for portfolio, then setLoad to generate slideshow
 
   return (
     <div className="portfolio-container">
-      <Zoom {...zoomInProperties}>
-      <div key={0} className="slide-div">
-        <h1 className="portfolio-title">Game Sleuth</h1>
-        <img className="slide-img" src={gamesleuth} alt="Game Sleuth" />
-      </div>
-      <div key={1} className="slide-div">
-        <h1 className="portfolio-title">Rancid Tomatillos</h1>
-        <img className="slide-img" src={rancid} alt="Rancid Tomatillos" />
-      </div>
-      <div key={2} className="slide-div">
-        <h1 className="portfolio-title">Project 3</h1>
-        <img className="slide-img" src={prague} alt="Portfolio" />
-      </div>
+      <Zoom scale={1.2}>
+        <div key={0} className="slide-div">
+          <h1 className="portfolio-title">Game Sleuth</h1>
+          <ReactPlayer
+            url="https://youtu.be/grPO7Bmcg-c"
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div key={1} className="slide-div">
+          <h1 className="portfolio-title">Rancid Tomatillos</h1>
+          <ReactPlayer
+            url="https://youtu.be/sXMR_tVFjh8"
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
+          />
+        </div>
+       
+        <div key={2} className="slide-div">
+          <h1 className="portfolio-title">Project 3</h1>
+          <img className="slide-img" src={prague} alt="Portfolio" />
+        </div>
       </Zoom>
     </div>
   );
