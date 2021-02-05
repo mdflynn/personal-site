295661f131d37ddc4c159d5743868f9250843069
mdflynn <mdflynn34@outlook.com>
2021-02-05 10:59:55 -0700

    add pdf download ability

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-
+import { Link } from "react-router-dom";
 import resume from "../../assets/resume.pdf";
 import "./Resume.scss";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Resume = () => {
   return (
-    <div className="pdf">
-      <Document file={resume} onLoadError={console.error}>
-        <Page pageNumber={1} />
-      </Document>
-    </div>
+    <main className="resume">
+      <h1>My Resume</h1>
+      <div className="pdf">
+        <Link to="Mike_Flynn_Resume.pdf" target="_blank" download>
+          Download Resume
+        </Link>
+        <Document file={resume} onLoadError={console.error}>
+          <Page pageNumber={1} />
+        </Document>
+      </div>
+    </main>
   );
 };
 
