8123a011216ea8d65e8999eeca997e80babfd5eb
mdflynn <mdflynn34@outlook.com>
2021-02-05 14:31:43 -0700

    Add glow animation

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -6,16 +6,29 @@ import "./Resume.scss";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Resume = () => {
+  // <button className="download-button">Download Mike's Resume</button>
   return (
     <main className="resume">
       <h1>My Resume</h1>
       <div className="pdf">
         <div className="download-div">
-          <p>Want a copy for yourself?<br /><br /><span>⬇⬇⬇</span></p>
-        <Link to="Mike_Flynn_Resume.pdf" target="_blank" download>
-          <p className="download-text">Download Mike's Resume</p>
-        </Link>
-        <span className="bottom-arrow">⬆⬆⬆</span> 
+          <p>Want a copy for yourself?</p>
+          <Link
+            className="link"
+            to="Mike_Flynn_Resume.pdf"
+            target="_blank"
+            download
+          >
+            <div className="glow-div">
+              <span>R</span>
+              <span>E</span>
+              <span>S</span>
+              <span>U</span>
+              <span>M</span>
+              <span>E</span>
+            </div>
+          </Link>
+          <span className="bottom-arrow">Click ⬆ to download</span>
         </div>
         <Document file={resume} onLoadError={console.error}>
           <Page pageNumber={1} />
