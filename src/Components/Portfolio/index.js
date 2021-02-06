77371730d61958eb585443af29eee1aae241298d
mdflynn <mdflynn34@outlook.com>
2021-02-06 10:49:23 -0700

    Add slap jack and coming soon

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player";
 import "./Portfolio.scss";
 import { Zoom } from "react-slideshow-image";
 
-import prague from "../../assets/night-prague.jpg";
+import black from "../../assets/black.jpg";
 
 const Portfolio = () => {
   //animation for portfolio, then setLoad to generate slideshow
@@ -12,7 +12,7 @@ const Portfolio = () => {
     <div className="portfolio-container">
       <h1 className="career-title">My Portfolio</h1>
       <h4>(Click title for more info)</h4>
-      <Zoom scale={1.2}>
+      <Zoom clasName="test" scale={1.2}>
         <div key={0} className="slide-div">
           <a
             className="portfolio-title"
@@ -23,6 +23,7 @@ const Portfolio = () => {
             Game Slueth
           </a>
           <ReactPlayer
+            className="slide-vid"
             url="https://youtu.be/grPO7Bmcg-c"
             playing={true}
             loop={true}
@@ -41,6 +42,7 @@ const Portfolio = () => {
             Rancid Tomatillos
           </a>
           <ReactPlayer
+            className="slide-vid"
             url="https://youtu.be/sXMR_tVFjh8"
             playing={true}
             loop={true}
@@ -49,10 +51,28 @@ const Portfolio = () => {
             height="100%"
           />
         </div>
-
         <div key={2} className="slide-div">
-          <h1 className="portfolio-title">Project 3</h1>
-          <img className="slide-img" src={prague} alt="Portfolio" />
+          <a
+            className="portfolio-title"
+            href="https://github.com/mdflynn/slapjack"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Slap Jack
+          </a>
+          <ReactPlayer
+            className="slide-vid"
+            url="https://youtu.be/In098DzQT00"
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div key={2} className="slide-div test">
+          <h1 className="sign">Coming<br />Soon</h1>
+          <img className="slide-img" src={black} alt="Portfolio" />
         </div>
       </Zoom>
     </div>
