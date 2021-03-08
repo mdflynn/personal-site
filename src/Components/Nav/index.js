565e408f2ba010356e83b60388727eccd71771b4
mdflynn <mdflynn34@outlook.com>
2021-03-08 16:33:19 -0700

    redesign nav

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Nav.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Nav.scss";
 
 const Nav = () => {
+  const toggleDropdown = (e) => {
+    e.preventDefault();
+    const navContainer = document.getElementById("myTopnav");
+    if (navContainer.className === "topnav") {
+      navContainer.className += " responsive";
+    } else {
+      navContainer.className = "topnav";
+    }
+  };
+
   return (
-    <nav>
-      <div className='logo-div'>
-        <p className='logo'>MF</p>
-      </div>
-      <ul>
-        <NavLink to='/' exact className='nav-link' activeClassName='active-nav'>
-          <li>Home</li>
-        </NavLink>
-        <NavLink to='/career' className='nav-link' activeClassName='active-nav'>
-          <li>Career</li>
-        </NavLink>
-        <NavLink
-          to='/portfolio'
-          className='nav-link'
-          activeClassName='active-nav'
-        >
-          <li>Portfolio</li>
-        </NavLink>
-        <NavLink
-          to='/resume'
-          className='nav-link'
-          activeClassName='active-nav'
-        >
-          <li>Resume</li>
-        </NavLink>
-        <NavLink
-          to='/contact'
-          className='nav-link'
-          activeClassName='active-nav'
-        >
-          <li>Contact</li>
-        </NavLink>
-      </ul>
+    <nav className="topnav" id="myTopnav">
+      <NavLink to="/" exact className="nav-link" activeClassName="active-nav">
+        Home
+      </NavLink>
+      <NavLink to="/career" className="nav-link" activeClassName="active-nav">
+        Career
+      </NavLink>
+      <NavLink
+        to="/portfolio"
+        className="nav-link"
+        activeClassName="active-nav"
+      >
+        Portfolio
+      </NavLink>
+      <NavLink to="/resume" className="nav-link" activeClassName="active-nav">
+        Resume
+      </NavLink>
+      <NavLink to="/contact" className="nav-link" activeClassName="active-nav">
+        Contact
+      </NavLink>
+      <a
+        href="/#"
+        style={{ fontSize: "15px" }}
+        className="icon"
+        onClick={(e) => toggleDropdown(e)}
+      >
+        &#9776;
+      </a>
     </nav>
   );
 };
