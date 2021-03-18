165d5ce8609a474ab70825f666bfbf1ec98c5c34
mdflynn <mdflynn34@outlook.com>
2021-03-18 12:37:54 -0600

    add media query structure

--- src/Components/AboutCard/index.js
+++ src/Components/AboutCard/index.js
@@ -5,16 +5,20 @@ const AboutCard = ({ title, description, img, alt }) => {
   return (
     <section className="about">
       <div className="text-container">
+        
         <img className="about-img" src={img} alt={alt} />
+
         <div className="overlay">
           <div className="about-p">
             <h1 className="career-title">{title}</h1>
             {description}
           </div>
         </div>
+
       </div>
     </section>
   );
 };
 
 export default AboutCard;
+// for mobile, add onClick to modal to change opacity
