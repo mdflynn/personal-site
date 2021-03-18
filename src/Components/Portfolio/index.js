a5c91b67b14fcc4042d453fc01e5a2ec599173e4
mdflynn <mdflynn34@outlook.com>
2021-03-18 13:12:14 -0600

    Add UFOMG to portfolio

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -13,6 +13,25 @@ const Portfolio = () => {
       <h1 className="career-title">My Portfolio</h1>
       <h4>(Click title for more info)</h4>
       <Zoom scale={1.2}>
+      <div key={0} className="slide-div">
+          <a
+            className="portfolio-title"
+            href="https://https://github.com/UFOMG/ufomg-fe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            UFOMG
+          </a>
+          <ReactPlayer
+            className="slide-vid"
+            url="https://youtu.be/3cCIJ7ht3as"
+            playing={true}
+            loop={true}
+            muted={true}
+            width="100%"
+            height="100%"
+          />
+        </div>
         <div key={0} className="slide-div">
           <a
             className="portfolio-title"
@@ -80,7 +99,19 @@ const Portfolio = () => {
         </div>
       </Zoom>
       <footer className="about-footer">
-        <Button />
+        <h3>
+          Feel free to checkout my{" "}
+          <a
+            className="email-link"
+            href="https://www.github.com/mdflynn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          profile to checkout more!
+        </h3>
+        <Button className="top-button" />Î
       </footer>
     </div>
   );
